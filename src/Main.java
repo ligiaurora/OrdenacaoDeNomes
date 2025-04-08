c753b22fe4d015f0cc6bf553772cd46829da24d2
Ligia Aurora <ligiaurora@gmail.com>
2025-04-08 10:13:43 -0300

    Adicionado duas Threads no Main com nome completo e incompleto na busca

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,18 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
     public static void main(String[] args) {
-        Semaphore sema = new Semaphore(2);
+        Semaphore semafaro = new Semaphore(2);
 
 
         for (int i = 0; i < 10; i++) {
-            Thread busca = new Thread(new ThreadDeBusca("arquivosNomes/nomescompletos-0" + i + ".txt", "Laerte", sema));
+            Thread busca = new Thread(new ThreadDeBusca("arquivosNomes/nomescompletos-0" + i + ".txt", "Laerte", semafaro));
+
+            busca.start();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Thread busca = new Thread(new ThreadDeBusca("arquivosNomes/nomescompletos-0" + i + ".txt", "Lig", semafaro));
+
             busca.start();
         }
     }
