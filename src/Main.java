8e2595e9ffe03c8116c1258482d60c8382f5cd13
Ligia Aurora <ligiaurora@gmail.com>
2025-04-08 10:08:53 -0300

    Mudanças nas classes

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Semaphore sema = new Semaphore(2);
 
+
         for (int i = 0; i < 10; i++) {
             Thread busca = new Thread(new ThreadDeBusca("arquivosNomes/nomescompletos-0" + i + ".txt", "Laerte", sema));
             busca.start();
