b02ac81732ea29d96cf8dd4282c5908a8009f15d
Ligia Aurora <ligiaurora@gmail.com>
2025-04-08 10:10:10 -0300

    semafaro

--- src/ThreadDeBusca.java
+++ src/ThreadDeBusca.java
@@ -2,19 +2,19 @@ import java.util.concurrent.Semaphore;
 
 public class ThreadDeBusca implements Runnable {
     private String nomeArquivo, valor;
-    private Semaphore semaphore;
+    private Semaphore semafaro;
 
-    public ThreadDeBusca(String nomeArquivo, String valor, Semaphore s1) {
+    public ThreadDeBusca(String nomeArquivo, String valor, Semaphore semafaro) {
         this.nomeArquivo = nomeArquivo;
         this.valor = valor;
-        this.semaphore = s1;
+        this.semafaro = semafaro;
     }
 
     @Override
     public void run() {
         BuscaNome buscador = new BuscaNome();
         try {
-            buscador.lerArquivo(nomeArquivo, valor, semaphore);
+            buscador.lerArquivo(nomeArquivo, valor, semafaro);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
