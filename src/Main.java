dad18233741ccf3e66a5351e9e89434828071d20
Ligia Aurora <ligiaurora@gmail.com>
2025-04-08 10:08:15 -0300

    Ajustes

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
     public static void main(String[] args) {
-        Semaphore s1 = new Semaphore(2);
+        Semaphore sema = new Semaphore(2);
 
         for (int i = 0; i < 10; i++) {
-            Thread busca = new Thread(new ThreadDeBusca("arquivosNomes/nomescompletos-0" + i + ".txt", "Laerte", s1));
+            Thread busca = new Thread(new ThreadDeBusca("arquivosNomes/nomescompletos-0" + i + ".txt", "Laerte", sema));
             busca.start();
         }
     }
--- src/ThreadDeBusca.java
+++ src/ThreadDeBusca.java
@@ -2,20 +2,19 @@ import java.util.concurrent.Semaphore;
 
 public class ThreadDeBusca implements Runnable {
     private String nomeArquivo, valor;
-    private Semaphore s1;
+    private Semaphore semaphore;
 
-    // Construtor corrigido
     public ThreadDeBusca(String nomeArquivo, String valor, Semaphore s1) {
         this.nomeArquivo = nomeArquivo;
         this.valor = valor;
-        this.s1 = s1;
+        this.semaphore = s1;
     }
 
     @Override
     public void run() {
         BuscaNome buscador = new BuscaNome();
         try {
-            buscador.lerArquivo(nomeArquivo, valor, s1);
+            buscador.lerArquivo(nomeArquivo, valor, semaphore);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
